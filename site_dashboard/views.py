582a147e3216faef01cbd99defb8e73b2f58023f
Mehjabin Mim <mehjabin.mim.0601@gmail.com>
2024-02-22 16:06:12 +0600

    Dynamic

--- site_dashboard/views.py
+++ site_dashboard/views.py
@@ -14,10 +14,54 @@ class MainDashBoardView(View):
         return render(request, "webpage/index.html")
 
 
+"""
+        ------------ Contact View ------------
+"""
 
 class SiteContactView(View):
     def get(self,request):
         return render(request, "webpage/contact.html")
+
+
+
+"""
+        ------------ Portfoilo Details View ------------
+"""
+
+class PortfolioDetailsView(View):
+    def get(self,request):
+        return render(request, "webpage/portfolio-details.html")
+
+
+
+
+"""
+        ------------ Blog View ------------
+"""
+
+class BlogSingleView(View):
+    def get(self,request):
+        return render(request, "webpage/blog-single.html")
+
+
+
+"""
+        ------------ 404 View ------------
+"""
+
+class Four0FourView(View):
+    def get(self,request):
+        return render(request, "webpage/404.html")
+
+
+
+"""
+        ------------ About Us View ------------
+"""
+
+class AboutUsView(View):
+    def get(self,request):
+        return render(request, "webpage/about-us.html")
     
 
 
